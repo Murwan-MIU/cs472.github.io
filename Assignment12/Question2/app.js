d58e071bb88db1a1abe39f85dda44c1858aa3d9c
Marwan <marwansalah44@gmail.com>
2023-04-25 14:25:09 -0500

    update assignment 12

--- Assignment12/Question2/app.js
+++ Assignment12/Question2/app.js
@@ -8,6 +8,12 @@ app.use(express.json());
 
 app.use('/courses', courseRouter);
 
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: 'Internal Server Error' });
+});
+
+
 app.listen(3000, () => {
     console.log('Your Server is running on 3000');
 });
